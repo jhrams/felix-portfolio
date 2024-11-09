d641027d26d149bc75d7c517b1b3220cbd8f274c
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-10 00:14:01 +0400

    fix: update folder structure for pages

---
+++ src/pages/Structure/index.tsx
@@ -0,0 +1,17 @@
+import {
+  STRUCTURE_TITLE,
+  RETAINER_MODEL,
+  SINGLE_PROJECT_MODEL,
+  TRIAL_MODEL
+} from '../../copies'
+
+const Structure = () => {
+  return (
+    <div>
+      <h1>{STRUCTURE_TITLE}</h1>
+      <p>This is the Structure page.</p>
+    </div>
+  )
+}
+
+export default Structure

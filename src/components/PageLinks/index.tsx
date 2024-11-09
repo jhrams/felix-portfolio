9777969a6ff3cf433b19b7e62212615c4f8502dd
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-10 00:26:14 +0400

    fix: add underline on pagelink based on current page

--- src/components/PageLinks/index.tsx
+++ src/components/PageLinks/index.tsx
@@ -1,11 +1,15 @@
+import { useLocation } from 'react-router-dom';
 import "./styles.css"
 
 const PageLinks = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <div>
-      <a href="/">Home</a>
-      <a href="/structure">Structure</a>
-      <a href="/contact">Contact</a>
+      <a href="/" className={currentPath === '/' ? 'active' : ''}>Home</a>
+      <a href="/structure" className={currentPath === '/structure' ? 'active' : ''}>Structure</a>
+      <a href="/contact" className={currentPath === '/contact' ? 'active' : ''}>Contact</a>
     </div>
   );
 }

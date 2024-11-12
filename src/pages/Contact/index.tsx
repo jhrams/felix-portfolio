bc4b79ed575c5655ccc7c6f4a5bb76f263eeba82
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-12 16:52:33 +0400

    fix: add mobile and desktop style for contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,12 +1,17 @@
+import { useNavbarHeight } from '../../context/NavbarHeightContext';
 import { GET_IN_TOUCH, CONTACT_DESCRIPTION } from '../../copies'
 import { renderText } from '../../utils/text';
 import './styles.css'
 
 const Contact: React.FC = () => {
+  const { navbarHeight } = useNavbarHeight();
+
   return (
     <div>
-      <h1>{GET_IN_TOUCH}</h1>
-      <p>{renderText(CONTACT_DESCRIPTION)}</p>
+      <div className='contact-title-container'>
+        <h1 className='contact-title' style={{ marginTop: `${navbarHeight}px` }}>{GET_IN_TOUCH}</h1>
+        <p className='contact-description'>{renderText(CONTACT_DESCRIPTION)}</p>
+      </div>
     </div>
   );
 }

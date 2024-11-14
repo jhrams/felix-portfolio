7a68ec78097ed68a79d93207f6db9308e993069c
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-15 02:46:05 +0400

    feat: add profile and contact button to profile header

--- src/pages/Home/ProfileHeader/index.tsx
+++ src/pages/Home/ProfileHeader/index.tsx
@@ -1,13 +1,16 @@
 import {
   HOME_TITLE,
   HOME_DESCRIPTION,
+  LETS_WORK_TOGETHER,
 } from '../../../copies';
 
 const ProfileHeader: React.FC = () => {
   return (
     <div className="profile-header">
+      <img src="/src/assets/profile-with-layer.png" alt="Profile" className="profile" />
       <h1>{HOME_TITLE}</h1>
       <p>{HOME_DESCRIPTION}</p>
+      <a href='/contact' className="contact-button">{LETS_WORK_TOGETHER}</a>
     </div>
   );
 }

38e6c980127b86a69bb6c7755d5d5887f4735849
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-14 22:52:00 +0400

    fix: add responsive styling to accordian

--- src/pages/Home/FrequentlyAskedQuestions/index.tsx
+++ src/pages/Home/FrequentlyAskedQuestions/index.tsx
@@ -3,15 +3,13 @@ import { FAQS, FAQ_TITLE } from "../../../copies";
 
 import "./styles.css"
 
-const FrequentlyAskedQuestions: React.FC = () => {
-  return (
-    <div className="faq">
-      <h2>{FAQ_TITLE}</h2>
-      {FAQS.map((faq, index) => (
-        <FAQItem key={index} question={faq.question} answer={faq.answer} />
-      ))}
-    </div>
-  );
-}
+const FrequentlyAskedQuestions: React.FC = () => (
+  <div className="faq">
+    <h2 className="faq-title">{FAQ_TITLE}</h2>
+    {FAQS.map(({ question, answer }, index) => (
+      <FAQItem key={index} question={question} answer={answer} />
+    ))}
+  </div>
+);
 
 export default FrequentlyAskedQuestions;

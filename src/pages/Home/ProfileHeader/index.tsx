f9d0aef95377dd1595d4aec86c57070060c30c65
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-16 00:38:04 +0400

    feat: add profile header style for all breakpoins

--- src/pages/Home/ProfileHeader/index.tsx
+++ src/pages/Home/ProfileHeader/index.tsx
@@ -1,17 +1,29 @@
+import useWindowWidth from '../../../hooks/useWindowWidth';
+
 import {
   HOME_TITLE,
   HOME_DESCRIPTION,
   LETS_WORK_TOGETHER,
 } from '../../../copies';
 
+import "./styles.css"
+
 const ProfileHeader: React.FC = () => {
+  const windowWidth = useWindowWidth();
+  const isDesktop = windowWidth > 1023;
+  const isTabletAndDesktop = windowWidth > 680;
+
   return (
-    <div className="profile-header">
-      <img src="/src/assets/profile-with-layer.png" alt="Profile" className="profile" />
-      <h1>{HOME_TITLE}</h1>
-      <p>{HOME_DESCRIPTION}</p>
-      <a href='/contact' className="contact-button">{LETS_WORK_TOGETHER}</a>
-    </div>
+    <>
+      <div className="profile-header">
+        <img src="/src/assets/profile-with-layer.png" alt="Profile" className="profile" />
+        <h1>{HOME_TITLE}</h1>
+        <p>{HOME_DESCRIPTION}</p>
+        <a href='/contact' className="contact-button">{LETS_WORK_TOGETHER}</a>
+        {isTabletAndDesktop && <img src="/src/assets/line.svg" alt="Line" className="line" />}
+        {isDesktop && <img src="/src/assets/profile-header-dots.png" alt="Profile-dots" className="profile-dots" />}
+      </div>
+    </>
   );
 }
 

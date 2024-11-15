48c9a0630c0fa77d60e61358cfa1d2d7ee40dda6
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-16 01:12:42 +0400

    fix: add dots to fractional contact component

--- src/pages/Home/FractionalContact/index.tsx
+++ src/pages/Home/FractionalContact/index.tsx
@@ -10,6 +10,8 @@ const FractionalContact: React.FC = () => (
   <div className="fractional-contact">
     <h2>{renderText(FRACTIONAL)}</h2>
     <a href="/contact">{LETS_WORK_TOGETHER}</a>
+    <img src="/src/assets/fractional-contact-left-dot.png" alt="Fractional-contact-left-dot" className="fractional-contact-left-dot" />
+    <img src="/src/assets/fractional-contact-right-dot.png" alt="Fractional-contact-right-dot" className="fractional-contact-right-dot" />
   </div>
 );
 

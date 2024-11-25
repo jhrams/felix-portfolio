0324e9e2915d26c631f3ead8c1075706e4264534
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-24 21:46:05 -0500

    fix: Fractional Contact wavy lines

--- src/pages/Home/FractionalContact/index.tsx
+++ src/pages/Home/FractionalContact/index.tsx
@@ -12,7 +12,8 @@ const FractionalContact: React.FC = () => (
     <h2>{renderText(FRACTIONAL)}</h2>
     <a href={`${BASE_URL}/#/contact`} className="button">{LETS_WORK_TOGETHER}</a>
     <img src="fractional-contact-left-dot.png" alt="Fractional-contact-left-dot" className="fractional-contact-left-dot" />
-    <img src="fractional-contact-right-dot.png" alt="Fractional-contact-right-dot" className="fractional-contact-right-dot" />
+    <img src="fractional-contact-l-right-dot.png" alt="Fractional-contact-l-right-dot" className="fractional-contact-l-right-dot" />
+    <img src="fractional-contact-xl-right-dot.png" alt="Fractional-contact-xl-right-dot" className="fractional-contact-xl-right-dot" />
   </div>
 );
 

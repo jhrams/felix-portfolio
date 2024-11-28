fb00ddc466e489c4fc213783030c5072415b58f5
Jorge Ramos <jorge.h.ramos.dev@gmail.com>
2024-11-27 19:59:52 -0500

    fix: update Home page pngs to svgs

--- src/pages/Home/FractionalContact/index.tsx
+++ src/pages/Home/FractionalContact/index.tsx
@@ -11,9 +11,9 @@ const FractionalContact: React.FC = () => (
   <div className="fractional-contact">
     <h2>{renderText(FRACTIONAL)}</h2>
     <a href={`${BASE_URL}/#/contact`} className="button">{LETS_WORK_TOGETHER}</a>
-    <img src="fractional-contact-left-dot.png" alt="Fractional-contact-left-dot" className="fractional-contact-left-dot" />
-    <img src="fractional-contact-l-right-dot.png" alt="Fractional-contact-l-right-dot" className="fractional-contact-l-right-dot" />
-    <img src="fractional-contact-xl-right-dot.png" alt="Fractional-contact-xl-right-dot" className="fractional-contact-xl-right-dot" />
+    <img src="fractional-contact-left-dot.svg" alt="Fractional-contact-left-dot" className="fractional-contact-left-dot" />
+    <img src="fractional-contact-l-right-dot.svg" alt="Fractional-contact-l-right-dot" className="fractional-contact-l-right-dot" />
+    <img src="fractional-contact-xl-right-dot.svg" alt="Fractional-contact-xl-right-dot" className="fractional-contact-xl-right-dot" />
   </div>
 );
 
--- src/pages/Home/ProfileHeader/index.tsx
+++ src/pages/Home/ProfileHeader/index.tsx
@@ -10,13 +10,13 @@ import "./styles.css"
 const ProfileHeader: React.FC = () => (
   <>
     <div className="profile-header">
-      <img src="profile-with-layer.png" alt="Profile" className="profile" />
+      <img src="profile-with-layer.svg" alt="Profile" className="profile" />
       <h1>{HOME_TITLE}</h1>
       <p>{HOME_DESCRIPTION}</p>
       <a href={`${BASE_URL}/#/contact`} className="button contact-button">{LETS_WORK_TOGETHER}</a>
       <img src="line.svg" alt="Line" className="line" />
-      <img src="profile-header-xl-dots.png" alt="Profile-xl-dots" className="profile-header-xl-dots" />
-      <img src="profile-header-l-dots.png" alt="Profile-l-dots" className="profile-header-l-dots" />
+      <img src="profile-header-xl-dots.svg" alt="Profile-xl-dots" className="profile-header-xl-dots" />
+      <img src="profile-header-l-dots.svg" alt="Profile-l-dots" className="profile-header-l-dots" />
     </div>
   </>
 );
